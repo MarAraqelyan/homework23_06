2a636b236d0bc20feb924675c7e1175e68114cec
Mariam <mariam.araqelyan2004@gmail.com>
2023-07-10 16:55:23 +0400

    observing the changes of the counter

---
+++ counter.py
@@ -0,0 +1,12 @@
+counter = 0
+
+def increment_counter():
+	global counter
+	counter = counter + 1 
+	print(counter)
+
+increment_counter()
+increment_counter()   
+increment_counter()
+increment_counter()
+
